a45df629c0342217db4105ec67713d2dd434113f
Kelin Christi <christi.kelin20@gmail.com>
2016-10-24 00:25:02 -0700

    Commented the code for better readability

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, render_template, jsonify
+# import the tweets.py module
 from tweets import Twitter
 
 app = Flask(__name__)
@@ -11,16 +12,23 @@ api = Twitter('@kelinc11')
 def strtobool(v):
     return v.lower() in ["yes", "true", "t", "1"]
 
+# First route that takes us to the home-page
 @app.route('/')
 def index():
+    # returns index page
     return render_template('index.html')
 
+# Second route that is dependent of the query and the rules made in tweets.py
 @app.route('/tweets')
 def tweets():
+    # requesting the rt_only method from tweets.py
     rt_only = request.args.get('rt_only')
+    # string to bool conv
     api.set_rt_checking(strtobool(rt_only.lower()))
+    # with_sent from tweets.py also
     with_sent = request.args.get('with_sent')
     api.set_with_sent(strtobool(with_sent.lower()))
+    # query from tweets.py
     query = request.args.get('query')
     api.setQuery(query)
 
